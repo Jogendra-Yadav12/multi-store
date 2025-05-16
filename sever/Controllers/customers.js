8142113e84249e1693dee367b1e2899a5faf73f6
Jogendra-Yadav12 <fireboyaj12@gmail.com>
2025-05-16 22:21:51 +0530

    commit

--- sever/Controllers/customers.js
+++ sever/Controllers/customers.js
@@ -40,6 +40,8 @@ export const loginCustomer = async (req, res) => {
   });
 };
 
+
+
 // Add Customer
 export const addCustomer = async (req, res) => {
   const { f_name, l_name, email, password, number } = req.body;
